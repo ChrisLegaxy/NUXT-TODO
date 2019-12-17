ac47631bf406e41d73ecbd428a2af20e42d1992c
Chris <chris_legaxy@yahoo.com>
2019-12-17 12:17:38 +0700

    Added a login layout for background image

---
+++ layouts/loginLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app style="background: url('https://pixelz.cc/wp-content/uploads/2018/10/blue-pink-gradient-uhd-4k-wallpaper.jpg')">
+    <nuxt />
+  </v-app>
+</template>
+
+<style>
+@media only screen and (min-width: 1200px) {
+  html {
+    height: 80vh;
+    overflow: auto;
+  }
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  /* box-shadow: inset 0 0 5px grey;  */
+  border-radius: 10px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #41b883;
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+/* ::-webkit-scrollbar-thumb:hover {
+  background: #41B883; 
+} */
+</style>
---
+++ pages/login/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container style="margin: auto;">
+    <v-row align="center" justify="center">
+      <v-card min-width="550px" raised shaped>
+        <v-card-title
+          class="font-weight-black white--text"
+          style="font-size: 24px; background-color: #41B883"
+          primary-title
+        >Nuxt TODO</v-card-title>
+        <div class="form-container">
+          <v-form @submit="login()">
+            <v-text-field type="text" placeholder="Username"></v-text-field>
+            <v-text-field type="password" placeholder="Password"></v-text-field>
+            <v-row style="margin-bottom: 15px;">
+              <v-col>
+                <v-btn color="primary">Login</v-btn>
+              </v-col>
+              <v-col class="text-right">
+                <v-btn color="error">Forget Password</v-btn>
+              </v-col>
+            </v-row>
+          </v-form>
+        </div>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  layout: 'loginLayout'
+};
+</script>
+<style>
+.form-container {
+  margin: 0 20px;
+}
+</style>
